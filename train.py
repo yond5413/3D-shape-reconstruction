e113d3cd61865b96e7dd6f59863988388cae53e0
yond5413 <yd2696@columbia.edu>
2024-04-23 21:52:21 -0400

    Update train.py

--- train.py
+++ train.py
@@ -44,6 +44,7 @@ def train(model,num_epochs,train_loader,val_loader,optimizer,configs):
             print(f"len of inputs: {len(inputs)}, data[0]: {len(data[0])}\n {type(data[0])} {data[0].size()}")
             print(f"data.size(): {data[1].size() }")
             print(f"voxel_grid len: {len(voxel_grids)}, type: {type(voxel_grids)}, {voxel_grids.size()}")
+            print(f"voxel[0].size(): {voxel_grids[0].size() } voxel[1].size(): {voxel_grids[1].size() }")
             ######################
             inputs = inputs.to(configs.device) ### should be cuda
             voxel_grids = voxel_grids.to(configs.device)
