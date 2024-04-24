905c62515b6e1333319d70500fd604ad3334f92c
yond5413 <yd2696@columbia.edu>
2024-04-23 22:09:43 -0400

    Update train.py

--- train.py
+++ train.py
@@ -52,6 +52,7 @@ def train(model,num_epochs,train_loader,val_loader,optimizer,configs):
             optimizer.zero_grad()
 
             outputs = model(inputs)
+            print(outputs.size())
             loss = criterion(outputs, voxel_grids)
             #loss.backward() NonGrad
             optimizer.step()
